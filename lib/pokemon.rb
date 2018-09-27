d3e1889592adafe1a3d7bab91b3140d8a0734eec
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-27 13:02:40 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,15 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def self.save(name, type , db)
+    db.execute("INSERT INTO pokemon (name, ) VALUES ();")
+  end
+  def self.find(poka, type , db)
+  
+  end
+  
 end
